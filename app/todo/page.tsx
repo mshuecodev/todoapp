33276e100fed889f063b9d8722d67f078d4ee0b9
misna04 <misnasyari@gmail.com>
2024-05-28 20:24:03 +0800

    todo app crud fixed

---
+++ app/todo/page.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import TodoList from "./TodoList"
+
+function TodoPage() {
+	return (
+		<div className="container mx-auto p-4">
+			<h1 className="text-4xl font-bold mb-6 text-center">Todo List</h1>
+			<TodoList />
+		</div>
+	)
+}
+
+export default TodoPage
